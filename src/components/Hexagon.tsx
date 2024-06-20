cb059cd5063059befaaf473138210fb62ca0db17
harshita2405 <harshita.singh@praetorian.com>
2024-06-20 23:31:37 +0530

    Upgrade modal

--- src/components/Hexagon.tsx
+++ src/components/Hexagon.tsx
@@ -1,5 +1,7 @@
 import React, { Component, ReactNode } from 'react';
 
+import { Notification } from '@/components/Notification';
+
 interface HexagonProps {
   notify?: boolean;
   children: ReactNode;
@@ -12,12 +14,7 @@ class Hexagon extends Component<HexagonProps> {
         <div className="hexagon">
           <div className="hexagon-content">{this.props.children}</div>
         </div>
-        {this.props.notify && (
-          <span className="absolute right-0 top-1 -mr-1 -mt-1 flex size-2">
-            <span className="absolute inline-flex size-full animate-ping rounded-full bg-brand-light opacity-75"></span>
-            <span className="relative inline-flex size-2 rounded-full bg-brand"></span>
-          </span>
-        )}
+        {this.props.notify && <Notification />}
       </div>
     );
   }
---
+++ src/hooks/useUpgrade.ts
@@ -0,0 +1,25 @@
+import { Snackbar } from '@/components/Snackbar';
+import { useAxios } from '@/hooks/useAxios';
+import { useMy } from '@/hooks/useMy';
+import { useMutation } from '@/utils/api';
+
+export const useUpgrade = () => {
+  const axios = useAxios();
+
+  const { invalidate } = useMy({ resource: 'account' }, { enabled: false });
+
+  return useMutation({
+    defaultErrorMessage: 'Failed to Upgrade account',
+    mutationFn: () => {
+      return axios.post('upgrade');
+    },
+    onSuccess: () => {
+      Snackbar({
+        title: 'Account Upgraded',
+        description: 'Successfully upgraded your account.',
+        variant: 'success',
+      });
+      invalidate();
+    },
+  });
+};
