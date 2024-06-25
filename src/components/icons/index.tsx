97c24cd8b75f5d989d1d4adcfca6da6290d87c0f
Geoff Storbeck <geoff.storbeck@praetorian.com>
2024-06-25 18:10:58 -0400

    update seed icon

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -1,11 +1,11 @@
 import {
-  CircleStackIcon,
+  GlobeAltIcon,
   CommandLineIcon,
   ExclamationTriangleIcon,
   ServerStackIcon,
 } from '@heroicons/react/24/outline';
 
-export const SeedsIcon = CircleStackIcon;
+export const SeedsIcon = GlobeAltIcon;
 export const AssetsIcon = ServerStackIcon;
 export const RisksIcon = ExclamationTriangleIcon;
 export const AttributesIcon = CommandLineIcon;
