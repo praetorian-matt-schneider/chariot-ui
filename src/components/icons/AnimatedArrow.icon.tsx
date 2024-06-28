5890f1c0b7c918bcb86655f8f2045b1470ad75ea
Geoff Storbeck <geoff.storbeck@praetorian.com>
2024-06-28 11:44:05 -0400

    lighten arrows

--- src/components/icons/AnimatedArrow.icon.tsx
+++ src/components/icons/AnimatedArrow.icon.tsx
@@ -4,9 +4,9 @@ import { cn } from '@/utils/classname';
 
 export const AnimatedArrowIcon = ({ className = '', delay = '0s' }) => (
   <div className={cn('icon-container relative', className)}>
-    <ArrowRightIcon className="absolute h-11 w-9 text-gray-300" />
+    <ArrowRightIcon className="absolute h-11 w-9 text-gray-200" />
     <ArrowRightIcon
-      className="icon-mask absolute h-11 w-9 text-gray-400"
+      className="icon-mask absolute h-11 w-9 text-gray-300"
       style={{ animationDelay: delay }}
     />
   </div>
