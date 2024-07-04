bb4881c8d3346a4601336482fc2aee01b5058e8c
Geoff Storbeck <geoff.storbeck@praetorian.com>
2024-07-04 10:03:04 -0400

    increase width

--- src/components/HorizontalSplit.tsx
+++ src/components/HorizontalSplit.tsx
@@ -9,7 +9,7 @@ export function HorizontalSplit(props: HorizontalSplitProps) {
   return (
     <div className="flex size-full gap-14">
       <div className="w-full">{props.leftContainer}</div>
-      <div className="w-1/3 min-w-[310px] shrink-0">{props.rightContainer}</div>
+      <div className="w-1/3 min-w-[350px] shrink-0">{props.rightContainer}</div>
     </div>
   );
 }
