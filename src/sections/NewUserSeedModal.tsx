8ddf87bed87806f2c020b30f61cfbd44712d3120
Harshita Singh <harshita.singh@praetorian.com>
2024-07-11 16:28:52 +0530

    Improves NUX code structure (#267)

--- src/sections/NewUserSeedModal.tsx
+++ src/sections/NewUserSeedModal.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react';
 import { CheckCircleIcon } from '@heroicons/react/24/solid';
-import { Radio, RadioGroup } from '@headlessui/react';
 
 import { Button } from '@/components/Button';
 import { ModalWrapper } from '@/components/Modal';
 import { useMy } from '@/hooks';
 import { useCreateAsset } from '@/hooks/useAssets';
 import { AssetStatus } from '@/types';
+import { cn } from '@/utils/classname';
 import { AllowedSeedRegex } from '@/utils/regex.util';
 import { StorageKey, useStorage } from '@/utils/storage/useStorage.util';
 
@@ -99,27 +99,25 @@ export const NewUserSeedModal = () => {
                 onChange={e => setSeedInput(e.target.value)}
                 className="block h-16 w-full rounded-l-[2px] bg-layer0 px-3 py-2 pr-[400px] text-xl font-bold shadow-sm focus:outline-none"
               />
-              <RadioGroup
-                value={assetStatus}
-                onChange={setAssetStatus}
-                aria-label="Server size"
-                className="mt-8 flex items-center justify-center gap-8"
-              >
-                {options.map(plan => (
-                  <Radio
-                    key={plan.value}
-                    value={plan.value}
-                    className="group relative flex w-[350px] cursor-pointer rounded-lg bg-white/5 px-5 py-4 text-white shadow-md transition focus:outline-none data-[checked]:bg-white/10 data-[focus]:outline-1 data-[focus]:outline-white"
+              <div className="m-auto my-8 flex justify-center gap-8">
+                {options.map(option => (
+                  <div
+                    key={option.value}
+                    className={cn(
+                      'group',
+                      assetStatus === option.value && 'is-selected'
+                    )}
+                    onClick={() => setAssetStatus(option.value)}
                   >
-                    <div className="flex w-full items-center justify-between">
-                      <div className="text-sm/6">
-                        <p className="font-semibold text-white">{plan.label}</p>
-                      </div>
-                      <CheckCircleIcon className="ml-4 size-6 fill-white opacity-0 transition group-data-[checked]:opacity-100" />
+                    <div className="relative flex w-[350px] cursor-pointer items-center justify-between gap-4 rounded-lg bg-header-dark px-5 py-4 text-layer0 shadow-md transition focus:outline-none group-[.is-selected]:bg-header-light data-[focus]:outline-1 data-[focus]:outline-layer0">
+                      <p className="text-sm/6 font-semibold text-layer0">
+                        {option.label}
+                      </p>
+                      <CheckCircleIcon className="size-6 fill-layer0 opacity-0 transition group-[.is-selected]:opacity-100" />
                     </div>
-                  </Radio>
+                  </div>
                 ))}
-              </RadioGroup>
+              </div>
             </div>
             <Button
               styleType="primary"
