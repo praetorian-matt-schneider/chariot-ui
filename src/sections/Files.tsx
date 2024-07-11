fe2c257d3f1d3fe1568100d30c117d36b948cf9c
Geoff Storbeck <geoff.storbeck@praetorian.com>
2024-07-11 10:07:19 -0400

    remove class

--- src/sections/Files.tsx
+++ src/sections/Files.tsx
@@ -1,6 +1,5 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { ChevronDownIcon } from '@heroicons/react/20/solid';
 import {
   ArrowDownCircleIcon,
   DocumentIcon,
@@ -10,7 +9,6 @@ import {
 } from '@heroicons/react/24/outline';
 
 import { Button } from '@/components/Button';
-import { Dropdown } from '@/components/Dropdown';
 import FileViewer from '@/components/FileViewer';
 import { RisksIcon } from '@/components/icons';
 import { Modal } from '@/components/Modal';
@@ -18,13 +16,9 @@ import { Table } from '@/components/table/Table';
 import { Columns } from '@/components/table/types';
 import { Tooltip } from '@/components/Tooltip';
 import { useDownloadFile, useMy } from '@/hooks';
-import { useCounts } from '@/hooks/useCounts';
-import { useFilter } from '@/hooks/useFilter';
 import { getDrawerLink } from '@/sections/detailsDrawer/getDrawerLink';
 import { useGlobalState } from '@/state/global.state';
-import { FileLabels, MyFile } from '@/types';
-import { useMergeStatus } from '@/utils/api';
-import { sortByDate } from '@/utils/date.util';
+import { MyFile } from '@/types';
 
 const Files: React.FC = () => {
   const navigate = useNavigate();
@@ -32,7 +26,7 @@ const Files: React.FC = () => {
   const [filename, setFilename] = React.useState('');
   const [filetype, setFiletype] = React.useState('');
   const {
-    status: fileStatus,
+    status,
     data: files = [],
     error,
     isFetchingNextPage,
@@ -41,52 +35,17 @@ const Files: React.FC = () => {
     resource: 'file',
     filterByGlobalSearch: true,
   });
-  const { data: stats = {}, status: statsStatus } = useCounts({
-    resource: 'file',
-    filterByGlobalSearch: true,
-  });
-
-  const status = useMergeStatus(fileStatus, statsStatus);
-
-  const [filter, setFilter] = useFilter('', 'file-class');
-
-  const filteredFiles = useMemo(() => {
-    if (filter) {
-      return files.filter((file: MyFile) => file.class === filter);
-    }
-    return files;
-  }, [filter, JSON.stringify(files)]);
 
   const {
     modal: {
       file: { onOpenChange: setIsUploadFileDialogOpen },
     },
   } = useGlobalState();
-  const sortedFiles = sortByDate(filteredFiles);
-  const filteredAndSortedFiles = sortedFiles.filter(
-    file => !file.name.endsWith('/')
-  );
 
   const { mutate: downloadFile } = useDownloadFile();
 
   function handleDownload(item: MyFile) {
     downloadFile({ name: item.name });
-  }
-
-  function getLabel(item: MyFile) {
-    const label = FileLabels[item.class];
-
-    // Remove pluralization
-
-    if (label) {
-      if (label.endsWith('s')) {
-        return label.slice(0, -1);
-      } else if (label.length) {
-        return label;
-      }
-    }
-
-    return item.class;
   }
 
   const getAdditionalActions = (item: MyFile) => {
@@ -149,14 +108,8 @@ const Files: React.FC = () => {
 
   const columns: Columns<MyFile> = [
     {
-      label: 'Class',
-      id: 'class',
-      className: 'w-1/5',
-      cell: (item: MyFile) => getLabel(item),
-    },
-    {
       label: 'Preview',
-      id: 'class',
+      id: 'name',
       className: 'w-20',
       cell: (item: MyFile) => getAdditionalActions(item),
     },
@@ -194,45 +147,12 @@ const Files: React.FC = () => {
     <div className="flex w-full flex-col">
       <Table
         columns={columns}
-        data={filteredAndSortedFiles}
+        data={files}
         resize={true}
         error={error}
         status={status}
         fetchNextPage={fetchNextPage}
         isFetchingNextPage={isFetchingNextPage}
-        filters={
-          <Dropdown
-            styleType="header"
-            label={filter ? `${FileLabels[filter]}` : 'All Documents'}
-            endIcon={
-              <ChevronDownIcon className="size-3 stroke-[4px] text-header-dark" />
-            }
-            menu={{
-              items: [
-                {
-                  label: 'All Classes',
-                  labelSuffix: files.length?.toLocaleString(),
-                  value: '',
-                },
-                {
-                  label: 'Divider',
-                  type: 'divider',
-                },
-                ...Object.entries(FileLabels).map(([key, label]) => {
-                  return {
-                    label,
-                    labelSuffix: stats[key]?.toLocaleString() || 0,
-                    value: key,
-                  };
-                }),
-              ],
-              onClick: value => {
-                setFilter(value || '');
-              },
-              value: filter,
-            }}
-          />
-        }
         primaryAction={() => {
           return {
             startIcon: <PlusIcon className="size-5" />,
