9639b2296bd3be92419542d7fe27e9635509777e
subbiah-dev <subbiah.chandramouli@praetorian.com>
2024-07-11 05:51:55 -0500

    upload text white uploading image (#266)
    * upload text
    
    * upload button
    
    ---------
    
    Co-authored-by: Geoff Storbeck <geoff.storbeck@praetorian.com>

--- src/components/markdown/MarkdownEditor.tsx
+++ src/components/markdown/MarkdownEditor.tsx
@@ -1,5 +1,8 @@
+import { useRef } from 'react';
+import { useDropzone } from 'react-dropzone';
+import { PhotoIcon } from '@heroicons/react/24/solid';
 import { PropsOf } from '@headlessui/react/dist/types';
-import MDEditor from '@uiw/react-md-editor';
+import MDEditor, { getCommands } from '@uiw/react-md-editor';
 import { v4 as uuidv4 } from 'uuid';
 
 import {
@@ -17,6 +20,8 @@ export function MarkdownEditor(
 ) {
   const { filePathPrefix = 'user-attachment' } = props;
 
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
   const { mutateAsync: updateFile } = useUploadFile();
 
   const [markdown, setMarkdown] = useStorage(
@@ -24,9 +29,119 @@ export function MarkdownEditor(
     ''
   );
 
+  const commands = getCommands();
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop: handleFileUpload,
+  });
+
+  const customCommands = commands.flatMap(command => {
+    if (command.keyCommand === 'image') {
+      return [
+        command,
+        {
+          name: 'addImage',
+          keyCommand: 'addImage',
+          render: () => (
+            <button className="p-1" {...getRootProps()}>
+              <input {...getInputProps()} />
+              <PhotoIcon className="-mt-px size-[14px] scale-110 " />
+            </button>
+          ),
+          shortcuts: 'ctrlcmd+k',
+          prefix: '![image](',
+          suffix: ')',
+          buttonProps: {
+            title: 'Click to Add image',
+          },
+        },
+      ];
+    }
+
+    return command;
+  });
+
+  async function handleFileUpload(files: File[]) {
+    const uploadingImages = files
+      .map((file): UploadingImage | undefined => {
+        if (file) {
+          if (/image\/.*/.test(file.type)) {
+            const id = uuidv4();
+            const src = `${filePathPrefix}/${id}`;
+
+            return {
+              markdownText: `![${file.name}](${AppMediaStoragePrefix}${src})`,
+              mutate: async () => {
+                await updateFile({
+                  ignoreSnackbar: true,
+                  name: src,
+                  content: await file.arrayBuffer(),
+                });
+              },
+            };
+          }
+        }
+      })
+      .filter(x => x) as UploadingImage[];
+
+    if (uploadingImages.length > 0) {
+      const uploadingImagesPromise = uploadingImages.map(i => i.mutate());
+      const uploadingImagesText = uploadingImages
+        .map(i => i.markdownText)
+        .join('\n');
+
+      const uploadingId = uuidv4();
+      const uploadingText = `Uploading ${uploadingId}`;
+
+      const textarea = (
+        textareaRef.current as unknown as { textarea: HTMLTextAreaElement }
+      )?.textarea as HTMLTextAreaElement;
+
+      if (textarea) {
+        const currentText = textarea.value || '';
+        const cursorPosition = textarea.selectionStart || 0;
+
+        // Insert the text at the cursor position
+        const newText =
+          currentText.slice(0, cursorPosition) +
+          uploadingText +
+          currentText.slice(cursorPosition);
+
+        // Update the textarea with the new text
+        setMarkdown(newText);
+
+        // Move the cursor to the end of the inserted text
+        textarea.selectionStart = textarea.selectionEnd =
+          cursorPosition + uploadingText.length;
+
+        await Promise.all(uploadingImagesPromise);
+
+        setMarkdown(text => {
+          return text.replace(uploadingText, uploadingImagesText);
+        });
+      }
+    }
+  }
+
+  async function handleDrop(event: React.DragEvent<HTMLDivElement>) {
+    if (event.dataTransfer.files.length > 0) {
+      event.preventDefault();
+
+      const files = Array(event.dataTransfer.items.length)
+        .fill(0)
+        .map((_, index): File | null => {
+          return event.dataTransfer.files.item(index);
+        })
+        .filter(x => x) as File[];
+
+      handleFileUpload(files);
+    }
+  }
+
   return (
     <MDEditor
       {...props}
+      ref={textareaRef}
       value={markdown}
       onChange={value => {
         setMarkdown(value || '');
@@ -37,57 +152,16 @@ export function MarkdownEditor(
           return <MarkdownPreview source={source} />;
         },
       }}
-      onDrop={async event => {
-        if (event.dataTransfer.files.length > 0) {
-          event.preventDefault();
-
-          const uploadingImagePromise = Array(event.dataTransfer.items.length)
-            .fill(0)
-            .map(async (_, index) => {
-              const file = event.dataTransfer.files.item(index);
-
-              if (file) {
-                const src = `${filePathPrefix}/${uuidv4()}`;
-
-                await updateFile({
-                  ignoreSnackbar: true,
-                  name: src,
-                  content: await file.arrayBuffer(),
-                });
-
-                return {
-                  src,
-                  alt: file.name,
-                };
-              }
-            });
-
-          const res = await Promise.all(uploadingImagePromise);
-
-          const textToInsert = res
-            .map(r => `![${r?.alt}](${AppMediaStoragePrefix}${r?.src})`)
-            .join('\n');
-          console.log('res', event.target);
-
-          const textarea = event.target as HTMLTextAreaElement;
-
-          const currentText = textarea.value;
-          const cursorPosition = textarea.selectionStart;
-
-          // Insert the text at the cursor position
-          const newText =
-            currentText.slice(0, cursorPosition) +
-            textToInsert +
-            currentText.slice(cursorPosition);
-
-          // Update the textarea with the new text
-          setMarkdown(newText);
-
-          // Move the cursor to the end of the inserted text
-          textarea.selectionStart = textarea.selectionEnd =
-            cursorPosition + textToInsert.length;
-        }
-      }}
+      commandsFilter={command =>
+        command.keyCommand !== 'image' ? command : false
+      }
+      commands={customCommands}
+      onDrop={handleDrop}
     />
   );
+}
+
+interface UploadingImage {
+  markdownText: string;
+  mutate: () => Promise<void>;
 }
