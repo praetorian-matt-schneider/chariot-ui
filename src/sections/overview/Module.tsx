277d7610f571e11de7f2c6f906451b7e96d06307
Geoff Storbeck <geoff.storbeck@praetorian.com>
2024-07-19 12:56:53 -0400

    styling

--- src/sections/overview/Module.tsx
+++ src/sections/overview/Module.tsx
@@ -16,7 +16,7 @@ export const Modules: Record<Module, Omit<ModuleMeta, 'risks' | 'status'>> = {
     Icon: BeakerIcon,
     label: 'Attack Surface Management',
     name: 'ASM',
-    description: `Attack surface management (ASM) refers to the proactive approach of identifying, analyzing, and managing potential points of attack on an organization&apos;s IT infrastructure, applications, and networks. The goal of ASM is to minimize the attack surface, which is the sum of all possible entry points that an attacker can exploit to gain unauthorized access or cause damage.`,
+    description: `Attack surface management (ASM) refers to the proactive approach of identifying, analyzing, and managing potential points of attack on an organization's IT infrastructure, applications, and networks. The goal of ASM is to minimize the attack surface, which is the sum of all possible entry points that an attacker can exploit to gain unauthorized access or cause damage.`,
     defaultTab: (
       <div className="p-4">
         <h3 className="text-2xl font-semibold">Attack Surface Management</h3>
@@ -25,6 +25,17 @@ export const Modules: Record<Module, Omit<ModuleMeta, 'risks' | 'status'>> = {
           discovery, identification, and monitoring of your evolving digital
           landscape.
         </p>
+        <p className="mt-2 text-default-light">
+          ASM helps you proactively manage potential vulnerabilities and
+          exposures, ensuring your organization&apos;s security measures keep
+          pace with the dynamic threat environment.
+        </p>
+        <a
+          href="#"
+          className="mt-4 inline-block rounded-sm border border-brand p-3 text-brand"
+        >
+          Learn More
+        </a>
       </div>
     ),
     integrations: [
@@ -41,24 +52,53 @@ export const Modules: Record<Module, Omit<ModuleMeta, 'risks' | 'status'>> = {
     label: 'Breach & Attack Simulation',
     name: 'BAS',
     description: `Breach and Attack Simulation (BAS) uses automated tools to continuously simulate real-world cyber attacks, helping organizations identify vulnerabilities, improve threat detection, and enhance their overall security posture.`,
-    defaultTab: <BasDefaultTab />,
+    defaultTab: (
+      <div className="p-4">
+        <h3 className="text-2xl font-semibold">Breach & Attack Simulation</h3>
+        <p className="mt-2 text-default-light">
+          BAS allows you to simulate real-world attack scenarios in a controlled
+          environment, identifying vulnerabilities before attackers can exploit
+          them.
+        </p>
+        <p className="mt-2 text-default-light">
+          Enhance your threat detection and response capabilities by
+          continuously testing your defenses against the latest attack
+          techniques.
+        </p>
+        <a
+          href="#"
+          className="mt-4 inline-block rounded-sm border border-brand p-3 text-brand"
+        >
+          Learn More
+        </a>
+      </div>
+    ),
     integrations: [],
   },
   EDR: {
     Icon: TrophyIcon,
     label: 'Endpoint Detection and Response',
     name: 'EDR',
-    description: `EDR provides essential cybersecurity benchmarks and guidelines, helping organizations protect against threats and improve their security posture.`,
+    description: `Endpoint Detection and Response (EDR) solutions provide continuous monitoring and response to advanced threats on endpoints, helping organizations detect and mitigate security incidents in real-time.`,
     defaultTab: (
       <div className="p-4">
         <h3 className="text-2xl font-semibold">
           Endpoint Detection and Response
         </h3>
         <p className="mt-2 text-default-light">
-          EDR provides essential benchmarks and guidelines to enhance your
-          organization&quot;s cybersecurity and improve defenses against
-          threats.
+          EDR solutions offer comprehensive visibility into endpoint activities,
+          enabling rapid detection and response to potential threats.
         </p>
+        <p className="mt-2 text-default-light">
+          Improve your security posture by leveraging advanced analytics and
+          automated responses to mitigate risks effectively.
+        </p>
+        <a
+          href="#"
+          className="mt-4 inline-block rounded-sm border border-brand p-3 text-brand"
+        >
+          Learn More
+        </a>
       </div>
     ),
     integrations: [Integrations.crowdstrike],
@@ -67,15 +107,25 @@ export const Modules: Record<Module, Omit<ModuleMeta, 'risks' | 'status'>> = {
     Icon: HomeIcon,
     label: 'Cyber Threat Intelligence',
     name: 'CTI',
-    description: `CTI collects and analyzes cyber threat information to help organizations proactively defend against and respond to attacks.`,
+    description: `Cyber Threat Intelligence (CTI) involves the collection and analysis of information about potential or current attacks that threaten an organization, helping to inform security decisions and proactive defense strategies.`,
     defaultTab: (
       <div className="p-4">
         <h3 className="text-2xl font-semibold">Cyber Threat Intelligence</h3>
         <p className="mt-2 text-default-light">
-          Chariot monitors emerging threats, including detailed analysis of new
+          Chariot monitors emerging threats, providing detailed analysis of new
           vulnerabilities, exploits, and attack vectors from a variety of
           trusted sources.
         </p>
+        <p className="mt-2 text-default-light">
+          Empower your organization with actionable intelligence to anticipate,
+          prepare for, and respond to evolving cyber threats.
+        </p>
+        <a
+          href="#"
+          className="mt-4 inline-block rounded-sm border border-brand p-3 text-brand"
+        >
+          Learn More
+        </a>
       </div>
     ),
     integrations: [],
@@ -84,15 +134,25 @@ export const Modules: Record<Module, Omit<ModuleMeta, 'risks' | 'status'>> = {
     Icon: TrophyIcon,
     label: 'Managed Service Provider',
     name: 'MSP',
-    description: `Our Vulnerability Management (VM) offers comprehensive IT services, including network, application, infrastructure, and security management, ensuring efficient and secure operation of your organization's IT systems.`,
+    description: `Our Managed Service Provider (MSP) offerings include comprehensive IT services such as network, application, infrastructure, and security management, ensuring efficient and secure operation of your organization's IT systems.`,
     defaultTab: (
       <div className="p-4">
-        <h3 className="text-2xl font-semibold">Workflows</h3>
+        <h3 className="text-2xl font-semibold">Managed Service Provider</h3>
         <p className="mt-2 text-default-light">
-          Vulnerability Management (VM) identifies and manages security
-          vulnerabilities in your IT infrastructure, integrating with various
-          tools for efficient tracking and resolution.
+          MSP services provide end-to-end IT management, including proactive
+          monitoring, maintenance, and support to ensure optimal performance and
+          security.
         </p>
+        <p className="mt-2 text-default-light">
+          Leverage our expertise to streamline your IT operations and focus on
+          your core business activities.
+        </p>
+        <a
+          href="#"
+          className="mt-4 inline-block rounded-sm border border-brand p-3 text-brand"
+        >
+          Learn More
+        </a>
       </div>
     ),
     integrations: [
@@ -106,15 +166,24 @@ export const Modules: Record<Module, Omit<ModuleMeta, 'risks' | 'status'>> = {
     Icon: TrophyIcon,
     label: 'Vulnerability Management',
     name: 'VM',
-    description: `Our Vulnerability Management (VM) offers comprehensive IT services, including network, application, infrastructure, and security management, ensuring efficient and secure operation of your organization's IT systems.`,
+    description: `Our Vulnerability Management (VM) services include identifying, assessing, and mitigating security vulnerabilities across your IT infrastructure to ensure robust protection against potential threats.`,
     defaultTab: (
       <div className="p-4">
         <h3 className="text-2xl font-semibold">Vulnerability Management</h3>
         <p className="mt-2 text-default-light">
-          Vulnerability Management (VM) identifies and manages security
-          vulnerabilities in your IT infrastructure, integrating with various
-          tools for efficient tracking and resolution.
+          VM services help you identify, prioritize, and remediate security
+          vulnerabilities to protect your organization from potential threats.
         </p>
+        <p className="mt-2 text-default-light">
+          Enhance your security posture by proactively managing vulnerabilities
+          across your IT infrastructure.
+        </p>
+        <a
+          href="#"
+          className="mt-4 inline-block rounded-sm border border-brand p-3 text-brand"
+        >
+          Learn More
+        </a>
       </div>
     ),
     integrations: [Integrations.nessus],
