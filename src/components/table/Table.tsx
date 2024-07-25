2ed5aea486e5a4ba8ccb9d83de9233aaeb0cdb34
Geoff Storbeck <geoff.storbeck@praetorian.com>
2024-07-25 09:45:50 -0400

    vim binding search

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -61,6 +61,7 @@ export function Table<TData>(props: TableProps<TData>) {
   } = props;
   const controlledSearchValue = controlledSearch?.value;
   const onControlledSearchChange = controlledSearch?.onChange;
+  const searchInputRef = useRef<HTMLInputElement>(null);
   const [search, setSearch] = useStorage(
     {
       parentState: controlledSearchValue,
@@ -312,6 +313,28 @@ export function Table<TData>(props: TableProps<TData>) {
     };
   }, [JSON.stringify({ selectedRows, rawData }), actions]);
 
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      const searchInput = document.getElementById('search') as HTMLInputElement;
+
+      if (event.key === '/') {
+        if (document.activeElement !== searchInput) {
+          event.preventDefault(); // Prevent the default browser action if input is not focused
+          if (searchInput) {
+            searchInput.focus();
+            searchInput.select(); // Optionally select the text in the input
+          }
+        }
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
   return (
     <Body
       ref={parentRef}
@@ -327,7 +350,7 @@ export function Table<TData>(props: TableProps<TData>) {
             <div className="flex space-x-4">
               <Input
                 name="search"
-                placeholder={`Go to ${tableName}`}
+                placeholder={'Type [/] to search'}
                 className="h-11 w-64 justify-end rounded-sm border-gray-900 bg-header-light p-2 text-sm text-white  ring-0"
                 value={search}
                 onChange={e => setSearch(e.target.value)}
