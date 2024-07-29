ee0d70042b2461be8aefa31e60913ade2232a39b
Geoff Storbeck <geoff.storbeck@praetorian.com>
2024-07-29 13:15:05 -0400

    style

--- src/sections/Alerts.tsx
+++ src/sections/Alerts.tsx
@@ -162,10 +162,10 @@ const Alerts: React.FC = () => {
     <div className="flex h-[60vh] shadow-sm">
       {/* Sidebar */}
       <div className="w-1/4 overflow-auto rounded-l-md rounded-r-none border border-r-0 border-gray-200 bg-zinc-50 bg-gradient-to-l py-4">
-        <h2 className="mb-2 flex items-center py-2 text-lg font-medium text-gray-800">
-          <Inbox className="ml-3 mr-2 size-8 " />
+        <h2 className="mb-4 flex items-center py-2 text-lg font-medium text-gray-800">
+          <Inbox className="ml-3 mr-2 size-8 stroke-[2px] " />
           {alerts && alerts.length && (
-            <span className="mr-2 text-2xl font-medium">
+            <span className="mr-2 text-xl font-bold">
               All Alerts ({totalAlerts})
             </span>
           )}
