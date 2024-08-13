c477fdead13c4d456b47a80cd47788f7d6848cbb
Noah Tutt <69209160+noah-tutt-praetorian@users.noreply.github.com>
2024-08-13 09:27:09 -0400

    Clarify the user signup errors (#492)

--- src/sections/signup/EmailPasswordForm.tsx
+++ src/sections/signup/EmailPasswordForm.tsx
@@ -43,7 +43,7 @@ export const EmailPasswordForm = ({
       <Inputs
         inputs={[
           {
-            label: 'Email Address',
+            label: 'Business Email Address',
             value: credentials.username,
             placeholder: 'janelongestname@praetorian.com',
             name: 'username',
@@ -72,7 +72,13 @@ export const EmailPasswordForm = ({
       >
         Continue
       </Button>
-      <SignupError error={error} />
+      <SignupError
+        error={
+          error
+            ? "Sign up failed. Please ensure you're using a business email address."
+            : error
+        }
+      />
       {isLogin && (
         <div>
           <div className="flex justify-between">
