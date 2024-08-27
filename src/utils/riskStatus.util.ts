cc21930715d37d60a2dfac1dea3daa66b9a75ffa
Geoff Storbeck <geoff.storbeck@praetorian.com>
2024-08-27 15:53:30 -0400

    rework statuses

---
+++ src/utils/riskStatus.util.ts
@@ -0,0 +1,9 @@
+import { RiskSeverity, RiskStatus } from '@/types';
+
+export const getRiskStatus = (status: string) => {
+  return status.slice(-1) as RiskStatus;
+};
+
+export const getRiskSeverity = (status: string) => {
+  return status.slice(0, -1) as RiskSeverity;
+};
