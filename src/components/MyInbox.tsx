8207a218e309dea1a558cd7ba57a7030483bd072
Harshita Singh <harshita.singh@praetorian.com>
2024-08-29 20:08:28 +0530

    Alerts fix (#605)

--- src/components/MyInbox.tsx
+++ src/components/MyInbox.tsx
@@ -10,9 +10,13 @@ import { getRoute } from '@/utils/route.util';
 import { StorageKey, useStorage } from '@/utils/storage/useStorage.util';
 
 const MyInbox: React.FC = () => {
-  const { data: alerts = [], isPending } = useGetAccountAlerts({
+  const { data: alertsWithAttributes = [], isPending } = useGetAccountAlerts({
     refetchInterval: sToMs(30),
   });
+  // This is temporary change till the time we have a proper way to handle attributes
+  const alerts = alertsWithAttributes?.filter(
+    ({ value }) => !value.startsWith('#attribute#')
+  );
 
   const [prevAlertCount, setPrevAlertCount] = useStorage<undefined | number>(
     { key: StorageKey.ALERT_COUNT },
