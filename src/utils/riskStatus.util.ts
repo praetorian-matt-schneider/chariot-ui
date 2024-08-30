63c2beff95f4047aa6f4584ea48f4bf4e550ea85
harshita2405 <harshita.singh@praetorian.com>
2024-08-30 17:07:29 +0530

    Table design updated

--- src/utils/riskStatus.util.ts
+++ src/utils/riskStatus.util.ts
@@ -2,15 +2,13 @@ import { RiskSeverity, RiskStatus } from '@/types';
 
 export const getRiskStatus = (status: string) => {
   // TODO : Confirm the logic
-  return [
-    RiskStatus.MachineOpen,
-    RiskStatus.MachineDeleted,
-    RiskStatus.ExposedRisks,
-  ].includes(status as RiskStatus)
-    ? (status as RiskStatus)
-    : (status.slice(0, -1) as RiskStatus);
+  return status.slice(0, -1) in RiskStatus
+    ? (status.slice(0, -1) as RiskStatus)
+    : (status as RiskStatus);
 };
 
 export const getRiskSeverity = (status: string) => {
-  return status.slice(-1) as RiskSeverity;
+  return status.slice(-1) in RiskSeverity
+    ? (status.slice(-1) as RiskSeverity)
+    : RiskSeverity.Info;
 };
