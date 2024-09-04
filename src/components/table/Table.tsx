6c33feaa4e6e0911e3561cfb1397e88b8b34b814
Geoff Storbeck <geoff.storbeck@praetorian.com>
2024-09-03 21:11:29 -0400

    tableid

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -1,7 +1,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* TODO: Fix the types for the Table component */
 
-import React, { ReactNode, useEffect, useMemo, useRef, useState } from 'react';
+import React, {
+  ReactNode,
+  useEffect,
+  useId,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
 import {
   ChevronDownIcon,
   ExclamationCircleIcon,
@@ -62,7 +69,7 @@ export function Table<TData>(props: TableProps<TData>) {
   const stickyRef = useCreateSticky<HTMLTableSectionElement>({
     id: 'table-header',
   });
-
+  const tableId = useId();
   const [expandedGroups, setExpandedGroups] = useState(
     groupBy?.map(group => group.label) || []
   );
@@ -108,7 +115,7 @@ export function Table<TData>(props: TableProps<TData>) {
     status,
   ]);
 
-  const parentRef = document.getElementById(isTableView ? 'body' : 'localBody');
+  const parentRef = document.getElementById(isTableView ? 'body' : tableId);
 
   useScroll(parentRef, fetchNextPage);
 
@@ -300,7 +307,7 @@ export function Table<TData>(props: TableProps<TData>) {
   }, [JSON.stringify({ selectedRows, rawData }), bulkActions]);
 
   return (
-    <div id="localBody">
+    <div id={tableId}>
       {(bodyHeader || parsedActions || parsedPrimaryAction) && (
         <RenderHeaderExtraContentSection>
           <div className=" flex flex-col justify-between gap-4 lg:flex-row">
